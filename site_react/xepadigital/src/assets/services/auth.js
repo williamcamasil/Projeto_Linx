aea44e39ecb55ee2adc45a1f63ebc19c1ee8cda0
FIC <FIC@info.local>
2019-11-28 18:00:15 -0200

    Pagina de Login

--- site_react/xepadigital/src/assets/services/auth.js
+++ site_react/xepadigital/src/assets/services/auth.js
@@ -1,6 +1,10 @@
+
+
 export const usuarioAutenticado = () => localStorage.getItem('usuario-xepa') !== null
 
 export const parseJwt = () => {
+
     var base64 = localStorage.getItem('usuario-xepa').split('.')[1]
+
     return JSON.parse(window.atob(base64))
 }
--- site_react/xepadigital/src/index.js
+++ site_react/xepadigital/src/index.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Inicial from './assets/pages/Inicial/Inicial';
 import * as serviceWorker from './serviceWorker';
+
+//DEPENDENCIAS
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
+
+//SERVICES
+
+//CSS
 import './assets/css/style.css';
 
-import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
+//PAGINAS
+import Inicial from './assets/pages/Inicial/Inicial';
 import Colaboradores from './assets/pages/Colaboradores/Colaboradores';
+import Login from './assets/pages/Login/Login';
+import { usuarioAutenticado, parseJwt } from './assets/services/auth';
+
+//AUTENTIFICAÇÃO
+// const PermissaoAdmin = ({ component : Component }) => (
+//     <Route 
+//         render={props =>
+//             usuarioAutenticado() && parseJwt().Role === "Administrador" ? (
+//                 <Component {...props}/>
+//             ) : (
+//                 <Redirect to={{ pathname : "/Login"}}/>
+//             )
+//         }
+//     />
+// )
+
+// const PermissaoCliente = ({ component : Component }) => (
+//     <Route 
+//         render={props =>
+//             usuarioAutenticado() && parseJwt().Role === "Cliente" ? (
+//                 <Component {...props}/>
+//             ) : (
+//                 <Redirect to={{ pathname : "/Login"}}/>
+//             )
+//         }
+//     />
+// )
 
 const Rotas = (
     //cria a URL
     <Router> 
         <div>
             <Switch>
-                <Route exact path = "/" component ={Inicial}/>
+                <Route exact path="/" component={Inicial}/>
+                {/* <PermissaoAdmin path="/" component={Inicial}/> */}
+                <Route path="/Login" component={Login}/>
                 <Route path="/Colaboradores" component = {() => <Colaboradores titulo_pagina="Colaboradores - XepaDigital" />}/>  
                 {/* <Route component = {NotFound}/> */}
             </Switch>
@@ -20,6 +56,8 @@ const Rotas = (
     </Router>
 )
 
+
+// TROCAMOS APP P/ NOSSAS ROTAS
 ReactDOM.render(Rotas, document.getElementById('root'));
 
 serviceWorker.unregister();
